83987434ea63b75121c0bc2b99efe90cde110cd9
m.adeel <m.adeel@epay.de>
2016-05-28 12:17:33 +0200

    first

---
+++ src/Add.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Class Test
+ */
+class Add
+{
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * @param $num1
+     * @param $num2
+     * @return int
+     */
+    public function add($num1 , $num2)
+    {
+        return ($num1 + $num2);
+    }
+
+}
