d77cf5a97302286890bf839e1fe1fe0bdc44e709
m.adeel <m.adeel@epay.de>
2016-05-28 12:27:51 +0200

    adding test

---
+++ Test/AddTest.php
@@ -0,0 +1,33 @@
+<?php
+#require (dirname(__FILE__) . '/../vendor/autoload.php');
+require(dirname(__FILE__) . '/../src/Add.php');
+/**
+ * Class AddTest
+ */
+class AddTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Add
+     */
+    private $model;
+
+    /**
+     *
+     */
+    public function setUp(){
+        $this->model = new Add();
+    }
+
+    /**
+     * AddTest constructor.
+     * @test
+     */
+    public function addTest()
+    {
+        $this->model = new Add();
+        $this->assertEquals($this->model->add(1,2 ),3);
+
+
+    }
+
+}
